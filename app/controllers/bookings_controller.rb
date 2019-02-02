c60b9bb54c090bd64f2ea94c4c1f5df1659d008e
github username <github email address>
2019-02-02 10:56:31 -0600

    refactored bookings views

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,10 +1,23 @@
 class BookingsController < ApplicationController
 
   def index
+    @booking = Booking.new
     @bookings = Booking.all
   end
 
   def show
+    @booking = Booking.find(params[:id])
   end
 
+  def create
+    #raise params.inspect
+    @booking = Booking.new
+    @booking.flight_id = params[:booking][:flight_id]
+    redirect_to booking_url(@booking)
+  end
+
+  private
+  def booking_params
+    params.require(:booking).permit(:flight_id)
+  end
 end
