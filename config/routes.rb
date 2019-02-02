a9aa5ccbd401d5dddf4826525aeb5d06eed3580d
github username <github email address>
2019-02-01 23:31:20 -0600

    added booking controller with routes and model

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  resources :bookings
   resources :flights
-  get 'site/index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'site#index'
+  root 'bookings#index'
 end
