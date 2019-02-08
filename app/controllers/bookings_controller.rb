f184092a9dcc4d662da96323533ec97a8b8ac272
github username <github email address>
2019-02-08 17:05:55 -0600

    more tweaking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -17,7 +17,7 @@ class BookingsController < ApplicationController
   def create
     @user = current_user
     #raise params.inspect
-    @booking = Booking.create(description: params[:description], paid: params[:booking][:paid], flight_id: params[:booking][:flight], user_id: params[:user_id])
+    @booking = Booking.create(description: params[:booking][:description], paid: params[:booking][:paid], flight_id: params[:booking][:flight], user_id: params[:user_id])
     #raise params.inspect
     #binding.pry
     if @booking.save
