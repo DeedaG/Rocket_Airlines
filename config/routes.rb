2586a8e9069913bb54cec9a796cfdbd093531f86
github username <github email address>
2019-02-11 14:34:21 -0600

    correct user route

--- config/routes.rb
+++ config/routes.rb
@@ -8,13 +8,14 @@ Rails.application.routes.draw do
     get '/sessions/destroy' => 'sessions#destroy'
     post '/signin' => 'sessions#create'
 
-    resources :users, only: [:show] do
-      resources :bookings, only: [:show, :index]
-    end
 
   resources :users
   resources :bookings
   resources :flights
+
+  resources :users do
+    resources :bookings, only: [:show, :index]
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
